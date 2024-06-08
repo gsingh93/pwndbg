a38601c683ef04dd9c61b13e8da1d8a4d2012bfa
Gulshan Singh <gsingh2011@gmail.com>
2024-06-08 02:48:43 -0700

    WIP: debug prints

--- gdbinit.py
+++ gdbinit.py
@@ -38,12 +38,13 @@ def update_deps():
     poetry_lock_hash_path = os.path.join(venv_path, "poetry.lock.hash")
     dev_marker_path = os.path.join(venv_path, "dev.marker")
 
+    print("here: 3")
     current_hash = calculate_hash(poetry_lock_path)
     stored_hash = None
     if os.path.exists(poetry_lock_hash_path):
         with open(poetry_lock_path, "r") as f:
             stored_hash = f.read().strip()
-
+    print("here: 4")
     # checks if dev.marker exists
     dev_mode = os.path.exists(dev_marker_path)
 
@@ -54,7 +55,8 @@ def update_deps():
             f.write(current_hash)
 
 
-if venv_path == "PWNDBG_PLEASE_SKIP_VENV" or path.exists(path.dirname(__file__) + "/.skip-venv"):
+if venv_path == "PWNDBG_PLEASE_SKIP_VENV" or path.exists(
+        path.dirname(__file__) + "/.skip-venv"):
     pass
 else:
     directory, file = path.split(__file__)
@@ -65,9 +67,18 @@ else:
         venv_path = os.path.join(directory, ".venv")
 
     if not os.path.exists(venv_path):
-        print(f"Cannot find Pwndbg virtualenv directory: {venv_path}: please re-run setup.sh")
+        print(
+            f"Cannot find Pwndbg virtualenv directory: {venv_path}: please re-run setup.sh"
+        )
         sys.exit(1)
-    update_deps()
+
+    print("here: 1")
+    try:
+        update_deps()
+    except Exception as e:
+        print(f"Error updating dependencies: {e}")
+        sys.exit(1)
+    print("here: 2")
 
     site_pkgs_path = glob(os.path.join(venv_path, "lib/*/site-packages"))[0]
 
